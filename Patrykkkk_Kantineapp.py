fa16f0c324cb0ca52458ba4ea84c9029dacc3435
Patryk08 <patrykmistak@gmail.com>
2026-06-10 12:26:06 +0200

    Laget en veiledning for brukeren
    La til tekst, endret på footer, css

--- Patrykkkk_Kantineapp.py
+++ Patrykkkk_Kantineapp.py
@@ -9,6 +9,7 @@ meny = [
     {"navn": "Pizza bit", "pris": 35 , "beskrivelse": "Sprø bund med masse ost, saus, sopp og korn", "bilde_url": "https://pizzabit.com.ua/img/21344994.jpg"},
     {"navn": "Burger", "pris": 60  , "beskrivelse": " Juicy burger med masse ingredienser som salat, agurt og kjøtt", "bilde_url": "https://tse4.mm.bing.net/th/id/OIP.Muo6k_4TwnZbpiYbKQt9QwHaE8?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3"},
     {"navn": "taco", "pris": 45   , "beskrivelse": " Fin mexicans taco med kjøtt, ost, sterk saus og korn", "bilde_url": "https://th.bing.com/th/id/R.29fcbeacb7943073b0f49676b4f4e3d8?rik=MTOXjA1QmE%2fNdA&pid=ImgRaw&r=0"},
+    {"navn": "Salat", "pris": 80   , "beskrivelse": " Salat med tomat, akurk og dressing", "bilde_url": "https://tse1.mm.bing.net/th/id/OIP.T1UaI6w94xOh7cfBBZtgaQHaF7?r=0&o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3"},
 
 ]
 
